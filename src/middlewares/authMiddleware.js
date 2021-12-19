2c1c3481e46f78bb8ac031b983e33d2bdbb3b7e4
Viet Huynh <huynhvanviet317@gmail.com>
2021-12-20 02:09:17 +0700

    [DONE] shoe detail popup and add to cart shoe feature

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -14,6 +14,7 @@ export const jwtMiddleware = async (req, res, next) => {
     try {
       const token = req.headers.authorization.split(" ")[1];
       const userData = await verifyToken(token, jwtSecret);
+      console.log(userData);
       req.user = userData;
       next();
     } catch (error) {
